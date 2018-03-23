b34a79d8f4447572a0a30e029e3dbe096e120158
ljhtxdy <ljhtxdynb@gmail.com>
2018-03-23 13:03:27 +0800

    Add files via upload

--- firstjava/src/firstjava/Stockanalysis.java
+++ firstjava/src/firstjava/Stockanalysis.java
@@ -3,8 +3,14 @@ package firstjava;
 import javax.swing.*;
 import java.awt.event.*;
 
+import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.StandardChartTheme;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
 import org.jsoup.select.Elements;
@@ -225,6 +231,8 @@ public class Stockanalysis implements ActionListener {
 		label.setFont(font);// 设置标签字体
 		label.setText("等待用户输入信息");// 设置默认显示文字
 		label1.setText("请输入股票代码：");
+		
+		chart=new ChartPanel(null);
 
 		panel.add(label1);
 		panel.add(textField, "centre");
@@ -236,7 +244,7 @@ public class Stockanalysis implements ActionListener {
 		frame.setContentPane(panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBounds(100, 100, 250, 250);
-		frame.setSize(700, 700);
+		frame.setSize(800, 600);
 		frame.setVisible(true);
 	}
 
@@ -251,10 +259,11 @@ public class Stockanalysis implements ActionListener {
 				e.printStackTrace();
 			}
 			if (disindex.equals("No")==false) {
+				panel.remove(chart);
 				button.setText("确定");
 				List<String> Data = run(code_, disindex);
-				graph another = new graph();
-				chart = new ChartPanel(another.createLineChart(code_, Data));
+				chart = new ChartPanel(createLineChart(code_, Data));
+				
 				panel.add(chart, "centre");
 				panel.repaint();
 			} else {
@@ -262,6 +271,39 @@ public class Stockanalysis implements ActionListener {
 			}
 
 		}
+	}
+	
+	public static JFreeChart createLineChart(String code,List<String> data) {		
+		//创建主题样式  
+        StandardChartTheme mChartTheme = new StandardChartTheme("CN");  
+        //设置标题字体  
+        mChartTheme.setExtraLargeFont(new Font("黑体", Font.BOLD, 20));  
+        //设置轴向字体  
+        mChartTheme.setLargeFont(new Font("宋体", Font.CENTER_BASELINE, 15));  
+        //设置图例字体  
+        mChartTheme.setRegularFont(new Font("宋体", Font.CENTER_BASELINE, 15));  
+        //应用主题样式  
+        ChartFactory.setChartTheme(mChartTheme);  
+
+		XYSeries series = new XYSeries("股票代码："+code);
+		for (int i = 0; i < data.size(); i++) {
+			float result = Float.parseFloat(data.get(data.size()-i-1));
+			series.add(i+1, result);
+		}
+
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		dataset.addSeries(series);
+		
+		JFreeChart chart = ChartFactory.createXYLineChart("近"+data.size()+"个交易日股价", "日期(天)", "股价(元)", dataset, PlotOrientation.VERTICAL, true,
+				true, true);
+
+		try {
+			ChartUtilities.saveChartAsPNG(new File("E:\\"+code+".png"), chart, 500, 400);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+        return chart;
 	}
 
 }
